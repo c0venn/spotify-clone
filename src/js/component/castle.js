345153790c8b06b0ccb92a630e71a0f5168f67d1
c0venn <fabichidori2@gmail.com>
2021-06-10 21:49:03 +0000

    unfinish

---
+++ src/js/component/castle.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+<audio controls>
+	<source src="https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3"></source>
+</audio>;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,23 @@
 import React from "react";
+import castle from "./castle";
+import hurry from "./hurry";
+import overworld from "./overworld";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="api-spotify">
+			<p>Hello World</p>
+			<ul>
+				<li>
+					<Castle />
+				</li>
+				<li>
+					<Hurry />
+				</li>
+				<li>
+					<Overworld />
+				</li>
+			</ul>
 		</div>
 	);
 }
