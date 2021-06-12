f0ddd6d02453db334bc957320359c30e246fc12f
c0venn <fabichidori2@gmail.com>
2021-06-12 18:29:52 +0000

    x terminar

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -21,5 +21,30 @@ function Home() {
         });
     }
 
+    useEffect(()=> { getFetch()});
     
+    let picksong = (index) => {
+        songPlayer.current.src = 'https://assets/breatheco.de/apis/sound/${songs[index].url}';
+    };
+    let playStop = () => {
+        songPlayer.current.play();
+    }
+    let pauseSong = () => {
+        songPlayer.current.pause();
+    }
+    return (
+        <>
+        <div className="row">
+            <div className="col-md-5 col-sm-7 m-auto px-0 mediaplayer">
+                <SongList songs={songs} currentSong={songPlayer} playStop={playStop} pickSong={pickSong}
+                pauseSong={pauseSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying} setSongOn={setSongOn}
+                songOn={songOn} />
+                <Player songs={songs} playStop={playStop} currentSong={songPlayer} pauseSong={pauseSong} isPlaying={isPlaying}
+                setIsPlaying={setIsPlaying} pickSong={pickSong} setSongOn={setSongOn} songOn={songOn} />
+            </div>
+        </div>
+        <audio src={(song[0] !== null && songs[0] !== undefined) ? "https://assets.breatheco.de/apis/sound/" + songs[0].url:""} ref={songPlayer}></audio>
+        </>    
+    );
 }
+export default Home;
