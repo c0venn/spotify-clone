a4967eae1f391e11b0579cb92cc9c51983abc0f5
c0venn <fabichidori2@gmail.com>
2021-06-15 16:39:49 +0000

    unfinished

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,19 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
-import  Player  from './player';
-import  Songs  from './temas';
+
 
 function Home() {
-   const [song, setSong] = useState('');
-   const [Playing, setPlaying] = useState(false);
-   const [currentsong, setCurrentSong] = useState(false)
-   const [isOn, setisOn] = useState(0);
-
-   let songPlayer = useRef(null);
-
+   
    let getFetch = () => {
        fetch("https://assets.breatheco.de/apis/sound/songs")
-        .then((result)=>{
-           return result.json();
+        .then((response) => {
+           return response.json();
         }).then((data) => {
            setSongs((data));
         }).catch((error) => {
@@ -21,24 +14,8 @@ function Home() {
         });
    };
 
-   useEffect(() => {
-       getFetch()
-   });
-
-    let pickSong = (index) => {
-        songPlayer.current.src = `https://assets.breatheco.de/apis/sound/${songs[index].url}`;
-    };
-
-    let playStop = () => {
-        songPlayer.current.play();
-    }
-
-    let pauseSong = () => {
-        songPlayer.current.pause();
-    }
-
     return (
-
+        <div className=""
     )
 }
 
